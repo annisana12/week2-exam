26905989ca82ff058a4784b5bc7dd912b46ac75b
annisana12 <98462398+annisana12@users.noreply.github.com>
2022-10-27 23:35:51 +0700

    publish exam

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Content({ content }) {
+  return (
+    <div className='box'>
+        <p>{content.title}</p>
+    </div>
+  )
+}
 
 function App() {
+  const [title, setTitle] = useState('');
+
+  const [todos, setTodos] = useState([{ id: 0, title: '' }]);
+
+  const onChangeHandler = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const onSubmitHandler = () => {
+    setTodos([...todos, { id: todos[todos.length - 1].id + 1, title: title }])
+    setTitle('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="input_container">
+        <input type="text" onChange={onChangeHandler} value={title} />
+        <button onClick={onSubmitHandler}>Add</button>
+      </div>
+      <h2>Todo List</h2>
+
+      <div className='box_container'>
+        {todos.map((todo) => {
+          if (todo.id > 0) {
+            return (<Content
+              content={todo}
+              key={`todo-${todo.id}`} />)
+          }
+        })}
+      </div>
+
     </div>
   );
 }
